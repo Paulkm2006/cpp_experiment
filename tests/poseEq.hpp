acc9ed5f197b3a09db3fb215c5bea4f2f4bee63c
Paul Liu <20290410+Paulkm2006@users.noreply.github.com>
2025-10-22 21:22:08 +0800

    test: split eq operator and fast tests

---
+++ tests/poseEq.hpp
@@ -0,0 +1,11 @@
+#include <gtest/gtest.h>
+#include <tuple>
+#include "Executor.hpp"
+
+namespace adas
+{
+	bool operator==(const Pose &lhs, const Pose &rhs)
+	{
+		return std::tie(lhs.x, lhs.y, lhs.heading) == std::tie(rhs.x, rhs.y, rhs.heading);
+	}
+}
