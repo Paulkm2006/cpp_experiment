9bc396cd9cd95ed9a4e84ecec43851a6b92a5c31
Paul Liu <20290410+Paulkm2006@users.noreply.github.com>
2025-10-22 19:38:59 +0800

    feat: add test for initial and default poses

---
+++ include/Executor.hpp
@@ -0,0 +1,24 @@
+#pragma once
+#include <string>
+namespace adas
+{
+	struct Pose
+	{
+		int x, y;
+		char heading;
+	};
+
+	class Executor
+	{
+	public:
+		static Executor *NewExecutor(const Pose &pose = {0, 0, 'N'}) noexcept;
+
+		Executor(void) = default;
+		virtual ~Executor(void) = default;
+
+		Executor(const Executor &) = delete;
+		Executor &operator=(const Executor &) = delete;
+
+		virtual Pose Query(void) const noexcept = 0;
+	};
+}
---
+++ src/ExecutorImpl.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "Executor.hpp"
+#include <string>
+
+namespace adas
+{
+	class ExecutorImpl : public Executor
+	{
+	public:
+		explicit ExecutorImpl(const Pose &pose) noexcept;
+		~ExecutorImpl() noexcept = default;
+
+		ExecutorImpl(const ExecutorImpl &) = delete;
+		ExecutorImpl &operator=(const ExecutorImpl &) = delete;
+		Pose Query(void) const noexcept override;
+
+	private:
+		/// @brief The current pose of the executor
+		Pose pose;
+	};
+
+} // namespace adas
